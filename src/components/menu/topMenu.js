bed7922a961f686370f3355989d05401560fe166
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-06-12 20:53:04 +0100

    fixed links

--- src/components/menu/topMenu.js
+++ src/components/menu/topMenu.js
@@ -1,30 +1,24 @@
 import React from 'react'
 import './topMenu.css';
-import {BrowserRouter, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const TopMenu = props => {
-
 
         return (
           
             <div className="ui topmenu stackable menu">
                     <div className="item">
-                          <BrowserRouter>
                         <NavLink to='/' exact={true} >
                             <i aria-hidden="true" className="home  icon 2x" ></i>
                             Home
                          </NavLink>
-                         </BrowserRouter>
                     </div>
                     <div className="item">
-                      <BrowserRouter>
                         <NavLink to='/examples' >
                             <i aria-hidden="true" className="lab  icon" ></i>
                             Examples
                         </NavLink>
-                        </BrowserRouter>
                     </div>
                 </div>
-              
       )
 }
 
