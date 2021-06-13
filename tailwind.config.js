e4d983c127c7cbcba7cccd24204c1c63dfdaee40
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-06-13 04:17:34 +0100

    updated theme dark/light

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+			translate: {
+				200: '200%',
+			},
   },
   variants: {
     extend: {},
